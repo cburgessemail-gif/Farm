ae32005ea09527ec39bd45a94110d4e44a8507ec
cburgessemail-gif <cburgessemail@gmail.com>
2026-04-14 13:27:28 -0400

    Replace App component with TestApp

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+function TestApp() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "black",
+        color: "yellow",
+        fontSize: "40px",
+        fontWeight: "bold",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      ROOT MAIN.TSX IS LOADING
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <TestApp />
   </React.StrictMode>
 );
